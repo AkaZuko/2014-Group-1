25296b3eff42e66fb7658ca5cfef9daf57402d23
sneha94 <snehakulkarni094@gmail.com>
2014-11-14 19:23:01 +0530

    Revert "Participant package updates"
    This reverts commit 8e022db8a1f2f4749b2c27c6499b2747518ea01d.

--- src/participant/ParticipantProfileFrame.java
+++ src/participant/ParticipantProfileFrame.java
@@ -1,81 +1,44 @@
 package participant;
 
-import java.awt.event.ActionEvent;
+import java.awt.EventQueue;
 
-public class ParticipantProfileFrame extends JFrame {
+import javax.swing.JFrame;
 
-	private String id;
+public class ParticipantProfileFrame {
 
-	// Create and set up the window.
+	private JFrame frame;
 
-	public ParticipantProfileFrame(final String id) {
-		this.id = id;
-
-		final Participant par = new Participant(id);
-		setTitle("Participant Profile");
-		getContentPane().setLayout(null);
-		setBounds(100, 100, 450, 300);
-
-		JButton btnNewButton_1 = new JButton("Update Profile");
-		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 11));
-
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
 				try {
-					par.updateProfile();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
+					ParticipantProfileFrame window = new ParticipantProfileFrame("");
+					window.frame.setVisible(true);
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				dispose();
 			}
 		});
-
-		btnNewButton_1.setBounds(48, 26, 147, 57);
-		getContentPane().add(btnNewButton_1);
-
-		JButton btnNewButton_2 = new JButton("Register an Event");
-		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				par.doEventRegPar();
-				dispose();
-			}
-		});
-		btnNewButton_2.setBounds(48, 104, 147, 57);
-		getContentPane().add(btnNewButton_2);
-
-		JButton btnNewButton = new JButton("View Dashboard");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				par.displayDash();
-				// dashFrame.setVisible(true);
-				dispose();
-			}
-		});
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNewButton.setBounds(48, 177, 147, 57);
-		getContentPane().add(btnNewButton);
-		final JPanel panel = new JPanel();
-
 	}
 
-	public static void main(String[] args) {
+	/**
+	 * Create the application.
+	 */
+	public ParticipantProfileFrame(String ID) {
+		initialize();
+		frame.setVisible(true);
+	}
 
-		final String id= "a";
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				createAndShowGUI();
-
-			}
-
-			private void createAndShowGUI() {
-
-				ParticipantProfileFrame ex = new ParticipantProfileFrame(id);
-				ex.setVisible(true);
-			}
-		});
-
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
